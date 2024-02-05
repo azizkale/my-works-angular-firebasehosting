70b793d716a2f4917cc082ea782fc6b992092d08
azizkale <azizkale@hotmail.com>
2024-02-05 11:15:01 +0100

    css setting added in the chapteredit.component.html file

--- src/app/group/piredit/chapteredit/chapteredit.component.ts
+++ src/app/group/piredit/chapteredit/chapteredit.component.ts
@@ -1,5 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { PireditService } from 'src/app/services/piredit.service';
 import { UserService } from 'src/app/services/user.service';
@@ -31,11 +36,11 @@ export class ChaptereditComponent implements OnInit {
   uid = localStorage.getItem('uid');
 
   selectedPirId: any;
-  selectedGroupId: any // to get users of this group
+  selectedGroupId: any; // to get users of this group
   selectedWord: any; // to edit word on chapter update form
   selectedChapter: Chapter;
-  users_createchapter: any[] = [] // fullfilling the select tag on FormGroup
-  users_updateform: any[] = [] // fullfilling the select tag on FormGroup
+  users_createchapter: any[] = []; // fullfilling the select tag on FormGroup
+  users_updateform: any[] = []; // fullfilling the select tag on FormGroup
 
   fontSize: number;
   lineHeight: number;
@@ -48,20 +53,18 @@ export class ChaptereditComponent implements OnInit {
     public userservice: UserService,
     public roleservice: RolesService,
     private displayService: DisplaypirService
-  ) {
-
-  }
+  ) {}
 
   ngOnInit(): void {
     this.selectedPirId = this.activeroute.snapshot.paramMap.get('pirid');
     this.selectedGroupId = this.activeroute.snapshot.paramMap.get('groupid');
     this.retrieveChapters();
-    this.createChapterRetrieveForm()
+    this.createChapterRetrieveForm();
     this.createNewChapterForm();
     this.createUpdateChapterForm();
     this.createAddWordPairForm();
 
-    this.roleControll(this.selectedGroupId, this.uid)
+    this.roleControll(this.selectedGroupId, this.uid);
     this.initialReadMode();
     this.initialFontSetting();
   }
@@ -80,25 +83,30 @@ export class ChaptereditComponent implements OnInit {
     this.createChapterForm = this.fb.group({
       chapterName: ['', Validators.required],
       chapterContent: ['', Validators.required],
-      selectEditor: ['', Validators.required]
+      selectEditor: ['', Validators.required],
     });
 
     // fullfilling the select tag on FormGroup
-    this.users_createchapter = []
-    this.userservice.retrieveAllUsersOfTheGroup(this.selectedGroupId).subscribe({
-      next: (ress: any) => {
-        ress.forEach((user: any) => {
-          this.users_createchapter.push(user)
-          this.createChapterForm.addControl(ress.uid, new FormControl(user.uid));
-        });
-      }
-    })
+    this.users_createchapter = [];
+    this.userservice
+      .retrieveAllUsersOfTheGroup(this.selectedGroupId)
+      .subscribe({
+        next: (ress: any) => {
+          ress.forEach((user: any) => {
+            this.users_createchapter.push(user);
+            this.createChapterForm.addControl(
+              ress.uid,
+              new FormControl(user.uid)
+            );
+          });
+        },
+      });
   }
 
   createAddWordPairForm() {
     this.addWordForm = this.fb.group({
       word: ['', Validators.required],
-      meaning: ['', Validators.required]
+      meaning: ['', Validators.required],
     });
   }
 
@@ -107,98 +115,131 @@ export class ChaptereditComponent implements OnInit {
       chapterId: ['', Validators.required],
       chapterName: ['', Validators.required],
       chapterContent: ['', Validators.required],
-      selectEditor: ['', Validators.required]
+      selectEditor: ['', Validators.required],
     });
 
     // fullfilling the select tag on FormGroup
-    this.users_updateform = []
-    this.userservice.retrieveAllUsersOfTheGroup(this.selectedGroupId).subscribe({
-      next: (ress: any) => {
-        ress.forEach((user: any) => {
-          this.users_updateform.push(user)
-          this.createChapterForm.addControl(ress.uid, new FormControl(user.uid));
-        });
-      }
-    })
+    this.users_updateform = [];
+    this.userservice
+      .retrieveAllUsersOfTheGroup(this.selectedGroupId)
+      .subscribe({
+        next: (ress: any) => {
+          ress.forEach((user: any) => {
+            this.users_updateform.push(user);
+            this.createChapterForm.addControl(
+              ress.uid,
+              new FormControl(user.uid)
+            );
+          });
+        },
+      });
   }
 
   retrieveChapters() {
     const editorId = this.uid;
-    this.roleservice.getUserRolesInTheGroup(this.selectedGroupId, editorId).subscribe({
-      next: (roles) => {
-        //if the user the editor, all chapter comes
-        if (roles?.includes(Roles[2])) {
-          this.pireditservice.retrieveAllChapters(this.selectedPirId).subscribe({
-            next: (ress) => {
-              if (ress !== undefined && ress !== null) {
-                this.chapters = Object.values(ress);
-                this.chapters.forEach((chapter, index) => {
-                  this.retrieveChapterForm.addControl(chapter.chapterName, new FormControl(chapter.chapterName));
-                });
-              }
-            }
-          })
-        }
-        else {
-          this.pireditservice.retrieveChaptersByEditorId(this.uid, this.selectedPirId).subscribe({
-            next: (ress) => {
-              if (ress !== undefined && ress !== null) {
-                this.chapters = Object.values(ress);
-                this.chapters.forEach((chapter, index) => {
-                  this.retrieveChapterForm.addControl(chapter.chapterName, new FormControl(chapter.chapterName));
-                });
-              }
-            }
-          })
-        }
-      }
-    })
+    this.roleservice
+      .getUserRolesInTheGroup(this.selectedGroupId, editorId)
+      .subscribe({
+        next: (roles) => {
+          //if the user the editor, all chapter comes
+          if (roles?.includes(Roles[2])) {
+            this.pireditservice
+              .retrieveAllChapters(this.selectedPirId)
+              .subscribe({
+                next: (ress) => {
+                  if (ress !== undefined && ress !== null) {
+                    this.chapters = Object.values(ress);
+                    this.chapters.forEach((chapter, index) => {
+                      this.retrieveChapterForm.addControl(
+                        chapter.chapterName,
+                        new FormControl(chapter.chapterName)
+                      );
+                    });
+                  }
+                },
+              });
+          } else {
+            this.pireditservice
+              .retrieveChaptersByEditorId(this.uid, this.selectedPirId)
+              .subscribe({
+                next: (ress) => {
+                  if (ress !== undefined && ress !== null) {
+                    this.chapters = Object.values(ress);
+                    this.chapters.forEach((chapter, index) => {
+                      this.retrieveChapterForm.addControl(
+                        chapter.chapterName,
+                        new FormControl(chapter.chapterName)
+                      );
+                    });
+                  }
+                },
+              });
+          }
+        },
+      });
   }
 
   addChapter(chapterName: string, chapterContent: string) {
     const editorId = this.createChapterForm.get('selectEditor')?.value;
 
     //chapterId will be given in server-side
-    const chapter = new Chapter(chapterName, chapterContent, null, editorId, this.selectedPirId, new Date(), [])
+    const chapter = new Chapter(
+      chapterName,
+      chapterContent,
+      null,
+      editorId,
+      this.selectedPirId,
+      new Date(),
+      []
+    );
     this.pireditservice.addChapter(chapter).subscribe({
       next: (ress) => {
-        console.log(ress)
+        console.log(ress);
       },
       complete: () => {
         this.retrieveChapters();
-      }
-    })
+      },
+    });
   }
 
   deleteChapter() {
-    this.pireditservice.deleteChapter(this.selectedPirId, this.updateChapterForm.get('chapterId')?.value).subscribe({
-      next: (ress) => {
-        this.retrieveChapters()
-        this.createChapterRetrieveForm()
-      }
-    })
+    this.pireditservice
+      .deleteChapter(
+        this.selectedPirId,
+        this.updateChapterForm.get('chapterId')?.value
+      )
+      .subscribe({
+        next: (ress) => {
+          this.retrieveChapters();
+          this.createChapterRetrieveForm();
+        },
+      });
   }
 
   selectChapter(chapter: Chapter) {
     this.selectedChapter = chapter;
     //modifies the chapter content adding <b> tag to wordpairs
-    if (chapter.chapterContent !== null || chapter.chapterContent !== undefined) {
+    if (
+      chapter.chapterContent !== null ||
+      chapter.chapterContent !== undefined
+    ) {
       if (chapter.wordPairs !== undefined) {
         for (const wordpair of Object.values(chapter.wordPairs)) {
           this.selectedChapterContentToEdit = chapter.chapterContent.replace(
             wordpair.word.trim(),
-            `<b>${wordpair.word.trim()}</b>`);
+            `<b>${wordpair.word.trim()}</b>`
+          );
         }
-      }
-      else {
+      } else {
         this.selectedChapterContentToEdit = chapter.chapterContent;
       }
-
     } else {
-      this.selectedChapterContentToEdit = chapter.chapterContent
+      this.selectedChapterContentToEdit =
+        'Bu bölüme henüz bir metin eklenmedi!';
     }
 
-    this.chapterContent.nativeElement.innerHTML = this.selectedChapterContentToEdit;
+    this.chapterContent.nativeElement.innerHTML =
+      this.selectedChapterContentToEdit;
 
     this.updateChapterForm = this.fb.group({
       chapterId: [chapter.chapterId],
@@ -207,27 +248,33 @@ export class ChaptereditComponent implements OnInit {
       pirId: [chapter.pirId, Validators.required],
       editorId: [chapter.editorId, Validators.required],
       createDate: [chapter.createDate, Validators.required],
-      selectEditor: [chapter.editorId]
+      selectEditor: [chapter.editorId],
     });
-
-
   }
 
   updateChapter() {
-    this.updateChapterForm.get('editorId')?.setValue(this.updateChapterForm.get('selectEditor')?.value)
+    this.updateChapterForm
+      .get('editorId')
+      ?.setValue(this.updateChapterForm.get('selectEditor')?.value);
 
     if (this.updateChapterForm.get('editorId')?.value !== '') {
-      this.pireditservice.updateChapter(this.updateChapterForm.value).subscribe({
-        next: (ress) => {
-          this.userservice.addRoleToUser(this.updateChapterForm.get('selectEditor')?.value, Roles[4], this.selectedGroupId).subscribe({
-            next: (resss) => {
-            }
-          })
-          this.retrieveChapters()
-        }
-      })
-    } else console.log('null editor id')
-
+      this.pireditservice
+        .updateChapter(this.updateChapterForm.value)
+        .subscribe({
+          next: (ress) => {
+            this.userservice
+              .addRoleToUser(
+                this.updateChapterForm.get('selectEditor')?.value,
+                Roles[4],
+                this.selectedGroupId
+              )
+              .subscribe({
+                next: (resss) => {},
+              });
+            this.retrieveChapters();
+          },
+        });
+    } else console.log('null editor id');
   }
 
   selectTextToManipulate() {
@@ -236,55 +283,62 @@ export class ChaptereditComponent implements OnInit {
   }
 
   saveWordPair(meaning: string) {
-    const wordPairId = Date.now().toString();// just to generate id
+    const wordPairId = Date.now().toString(); // just to generate id
     const wordPair = new WordPair(
       wordPairId,
       this.selectedWord,
       meaning,
       this.updateChapterForm.get('chapterId')?.value,
       this.updateChapterForm.get('pirId')?.value,
-      this.uid)
+      this.uid
+    );
 
     //creating wordpair
     this.pireditservice.createWordPair(wordPair).subscribe({
       next: (ress) => {
         this.updateChapter(); // to save (as updated) the word that be made bold
-      }, complete: () => {
-        this.createAddWordPairForm();// to clear the form
+      },
+      complete: () => {
+        this.createAddWordPairForm(); // to clear the form
         this.retrieveChapters();
-        this.wordpaireditComponent.retrieveAllWordPairsOfSinglePir()
+        this.wordpaireditComponent.retrieveAllWordPairsOfSinglePir();
         this.getChapterByChapterId();
-
-      }
-    })
-
+      },
+    });
   }
 
   roleControll(groupId: any, userId: any) {
     this.roleservice.getUserRolesInTheGroup(groupId, userId).subscribe({
       next: (roles) => {
-        this.allowedToAdminAndMentor = roles.includes(Roles[1]) || roles.includes(Roles[2])
+        this.allowedToAdminAndMentor =
+          roles.includes(Roles[1]) || roles.includes(Roles[2]);
         this.allowedToAdmin = roles.includes(Roles[1]);
-      }
-    })
+      },
+    });
   }
 
   getChapterByChapterId() {
     //to updated after saveing wordpair
-    this.displayService.retrieveChapterByChapterId(this.selectedChapter.chapterId, this.selectedPirId).subscribe({
-      next: (chapter: Chapter) => {
-        if (chapter.wordPairs !== undefined) {
-          for (const wordpair of Object.values(chapter.wordPairs)) {
-            this.selectedChapterContentToEdit = chapter.chapterContent.replace(
-              wordpair.word.trim(),
-              `<b>${wordpair.word.trim()}</b>`);
+    this.displayService
+      .retrieveChapterByChapterId(
+        this.selectedChapter.chapterId,
+        this.selectedPirId
+      )
+      .subscribe({
+        next: (chapter: Chapter) => {
+          if (chapter.wordPairs !== undefined) {
+            for (const wordpair of Object.values(chapter.wordPairs)) {
+              this.selectedChapterContentToEdit =
+                chapter.chapterContent.replace(
+                  wordpair.word.trim(),
+                  `<b>${wordpair.word.trim()}</b>`
+                );
+            }
+          } else {
+            this.selectedChapterContentToEdit = chapter.chapterContent;
           }
-        }
-        else {
-          this.selectedChapterContentToEdit = chapter.chapterContent;
-        }
-      }
-    });
+        },
+      });
   }
 
   increaseFontSize() {
@@ -325,7 +379,6 @@ export class ChaptereditComponent implements OnInit {
 
   readModeClass(): string {
     return this.isNightMode ? 'night-mode' : 'light-mode';
-
   }
 
   initialReadMode() {
@@ -338,7 +391,10 @@ export class ChaptereditComponent implements OnInit {
 
   changeReadMode() {
     this.isNightMode = !this.isNightMode;
-    localStorage.setItem('readMode', this.isNightMode ? 'night-mode' : 'light-mode');
+    localStorage.setItem(
+      'readMode',
+      this.isNightMode ? 'night-mode' : 'light-mode'
+    );
     this.readModeClass();
   }
 }
