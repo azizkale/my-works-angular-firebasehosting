88d039589f83c9f084a047c159da4de464e4fc47
azizkale <azizkale@hotmail.com>
2024-02-21 09:59:45 +0100

    environment changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  url: 'http://localhost:3001',
-  // url: 'http://89.58.18.182:3001',
+  // url: 'http://localhost:3001',
+  url: 'http://89.58.18.182:3001',
   // url: 'https://angry-turtleneck-shirt-bee.cyclic.app',
 };
