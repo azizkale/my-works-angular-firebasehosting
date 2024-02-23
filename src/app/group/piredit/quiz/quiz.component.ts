4cf28a01e524f9414a26d464f50445f5371f505e
azizkale <azizkale@hotmail.com>
2024-02-23 11:32:30 +0100

    radio butoon added to question form

--- src/app/group/piredit/quiz/quiz.component.ts
+++ src/app/group/piredit/quiz/quiz.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz',
@@ -11,6 +11,21 @@ export class QuizComponent implements OnInit {
   constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.initiateQuestionForm()
   }
 
+  initiateQuestionForm(){
+    this.addQuizQuestion = this.fb.group({
+      question: ['', Validators.required],
+      answer_1: ['', Validators.required],
+      answer_2: ['', Validators.required],
+      answer_3: ['', Validators.required],
+      answer_4: ['', Validators.required],
+      answer: [null, Validators.required],
+    });
+  }
+
+  save(form:FormGroup){
+console.log(form)
+  }
 }
