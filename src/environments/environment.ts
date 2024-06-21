139090235d404c9f7a3c380ea4d353d2cc9a0fa4
kaleaziz <aziz.kale@emm-it.de>
2024-06-21 12:07:00 +0200

    global ip added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  url: 'http://192.168.0.94:3001',
+  url: 'http://172.214.122.92',
   // url: 'http://89.58.18.182:3001',
   // url: 'https://cute-teal-colt-robe.cyclic.app',
 };
