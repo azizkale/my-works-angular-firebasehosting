79f811cfcb4eb6fcf68082597e2e3a1e1e132cbd
kaleaziz <aziz.kale@emm-it.de>
2024-06-25 15:10:04 +0200

    ip added from k8s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:3001',
-  // url: 'http://89.58.18.182:3001',
-  // url: 'https://cute-teal-colt-robe.cyclic.app',
+  url: 'http://4.156.80.52',
 };
